0470a0d0ea4f777bfc41582781783d3728fdb232
Mark Linn <marktlinn@gmail.com>
2022-08-22 17:53:09 +0200

    added basic authentication checking middleware to the routes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const { ensureAuth, ensureGuest} = require('../middleware/auth')
 
-
-router.get('/', (req,res)=>{
+// Login screen
+router.get('/', ensureGuest, (req,res)=>{
     res.render('login', {
         layout: 'login',
     })
 })
 
-router.get('/dashboard', (req,res)=>{
+// dashboard accessed after login
+router.get('/dashboard', ensureAuth, (req,res)=>{
     res.render('dashboard')
 })
 
