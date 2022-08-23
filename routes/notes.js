7413770ac782f880ad0172e0543a8c4cec4aed6c
Mark Linn <marktlinn@gmail.com>
2022-08-23 15:58:32 +0200

    added edit and delete buttons to dashboard, updated notes route: everything inside a try/catch now

--- routes/notes.js
+++ routes/notes.js
@@ -40,42 +40,67 @@ router.get('/', ensureAuth, async (req,res)=>{
 
 // Edit notes page
 router.get('/edit/:id', ensureAuth, async (req,res)=>{
-    const notes = await Notes.findOne({
-        _id: req.params.id
-    }).lean()
+    try{
+        const notes = await Notes.findOne({
+            _id: req.params.id
+        }).lean()
 
-    if(!notes) {
-        return res.render('error/404')
-    }
+        if(!notes) {
+            return res.render('error/404')
+        }
 
-    if(notes.user != req.user.id){
-        res.redirect('/notes')
+        if(notes.user != req.user.id){
+            res.redirect('/notes')
+        }
+        else{
+            res.render('notes/edit', {
+                notes,
+            })
+        }
     }
-    else{
-        res.render('notes/edit', {
-            notes,
-        })
+    catch (err){
+        console.error(err)
+        return res.render('error/500')
     }
 })
 
 //Update note
 router.put('/:id', ensureAuth, async (req,res)=>{
-    let notes = await Notes.findById(req.params.id).lean()
+    try{
+        let notes = await Notes.findById(req.params.id).lean()
 
-    if(!notes){
-        return res.render('error/404');
-    }
+        if(!notes){
+            return res.render('error/404');
+        }
 
-    if(notes.user != req.user.id){
-        res.redirect('/notes')
+        if(notes.user != req.user.id){
+            res.redirect('/notes')
+        }
+        else{
+            notes = await Notes.findOneAndUpdate({_id: req.params.id}, req.body,
+            {
+                new: true,
+                runValidators: true,
+            })
+            res.redirect('/dashboard')
+        }
     }
-    else{
-        notes = await Notes.findOneAndUpdate({_id: req.params.id}, req.body,
-        {
-            new: true,
-            runValidators: true,
-        })
-        res.redirect('/dashboard')
+    catch (err){
+        console.error(err)
+        return res.render('error/500')
     }
 })
+
+// Delete Route
+router.delete('/:id', ensureAuth, async (req,res)=>{
+    try{
+        await Notes.remove( {_id: req.params.id} )
+        res.redirect('/dashboard')
+    }
+    catch (err){
+        console.error(err)
+        return res.render('error/500')
+    }
+})
+
 module.exports = router
